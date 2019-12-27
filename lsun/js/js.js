903afb4c021884cb73c0fe002a0f4d4d6ffd6f98
lsun41902 <blossom41902@gmail.com>
2019-12-27 17:49:00 +0900

    commit

--- lsun/js/js.js
+++ lsun/js/js.js
@@ -7,10 +7,53 @@ $(document).ready(function(){
     
 });
 $(document).ready(function(){
+    $("#gongji").hover(function(){
+      $(this).css("background-color", "#2e1705");
+      }, function(){
+      $(this).css("background-color", "#2e1705");
+    });
+  });
+
+  $(document).click(function(){
+    //input 요소의 포커스 이벤트 처리
+    $('#gongji').focus(function(){
+        if($("#gongji1").css("display") == "none"){   
+            jQuery('#gongji1').css("display", "block");   
+            jQuery('#event1').css("display", "none");
+        } else {  
+            jQuery('#event1').css("display", "none");   
+        } 
+    });
+     
+});
+
+$(document).ready(function(){
+    //input 요소의 포커스 이벤트 처리
+    $('#event').click(function(){
+        if($("#event1").css("display") == "none"){   
+            jQuery('#event1').css("display", "block");  
+            jQuery('#gongji1').css("display", "none");
+        } else {  
+            jQuery('#gongji1').css("display", "none");   
+        }  
+    });
+    
+});
+
+
+
+$(document).ready(function(){
     //input 요소의 포커스 이벤트 처리
     $('#event').focus(function(){
         $(this).css('background-color','#2e1705');
         $('#gongji').css('background-color','#a97b48');
     });
-    
 });
+$(document).ready(function(){
+    $("#event").hover(function(){
+      $(this).css("background-color", "#2e1705");
+      
+      }, function(){
+      $(this).css("background-color", "#2e1705");
+    });
+  });
